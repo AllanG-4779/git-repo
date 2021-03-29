f31a3ddcd61c45a6ba0497dff018bc63f24f1923
Allan-Onyango <58295678+Allan-Onyango@users.noreply.github.com>
2021-03-29 12:26:35 +0300

    Comments
    Comments were added to the master file

--- BinaryToDecimal.js
+++ BinaryToDecimal.js
@@ -1,4 +1,5 @@
 var binNumber = "";
+
 const Binary = (number) => {
   if (number === 0) {
     return;
@@ -6,12 +7,12 @@ const Binary = (number) => {
     let remainder = number % 2;
 
     number = Math.floor(number / 2);
-binNumber += remainder;
+    binNumber += remainder;
 
     Binary(number);
   }
 };
-
+//This method iterates through the string and prints out in reverse order
 Binary(76540);
 var binaryEquiv = "";
 for (var i = binNumber.length - 1; i >= 0; i--) {
