24874f780d21be41681eb55b94dd6fb82fce4e9e
Allan-Onyango <58295678+Allan-Onyango@users.noreply.github.com>
2021-03-29 10:37:39 +0300

    descriptive update
    the changes were made to make the code more descriptive and to increase readability

---
+++ BinaryToDecimal.js
@@ -0,0 +1,20 @@
+var binNumber = "";
+const Binary = (number) => {
+  if (number === 0) {
+    return;
+  } else {
+    let remainder = number % 2;
+
+    number = Math.floor(number / 2);
+binNumber += remainder;
+
+    Binary(number);
+  }
+};
+
+Binary(76540);
+var binaryEquiv = "";
+for (var i = binNumber.length - 1; i >= 0; i--) {
+  binaryEquiv += binNumber[i];
+}
+console.log(binaryEquiv);
